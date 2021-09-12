180e5f271a640ff9655359f4ba4f95740ea1ec74
Gaurip10 <thegauriparhad@gmail.com>
2021-09-12 11:02:39 +0530

    Added: Add contact

--- src/main/java/bridgelabz/com/ContactCreation.java
+++ src/main/java/bridgelabz/com/ContactCreation.java
@@ -62,4 +62,41 @@ public class ContactCreation {
         } catch (Exception e) {
             System.out.println(e);
         }
+        
+        
+        public void addContact() {
+            try {
+                System.out.println("Enter following details \n" +
+                        "First Name :");
+                String firstName = scanner.nextLine();
+                System.out.println("Last Name :");
+                String lastName = scanner.nextLine();
+                System.out.println("Address :");
+                String address = scanner.nextLine();
+                System.out.println("City :");
+                String city = scanner.nextLine();
+                System.out.println("State :");
+                String state = scanner.nextLine();
+                System.out.println("Zip Code :");
+                int zip = scanner.nextInt();
+                System.out.println("Phone Number :");
+                long phoneNo = scanner.nextLong();
+                System.out.println("Email :");
+                String email = scanner.next();
+
+                ContactInfo contactInfo;
+                contactInfo.setFirstName(firstName);
+                contactInfo.setLastName(lastName);
+                contactInfo.setAddress(address);
+                contactInfo.setCity(city);
+                contactInfo.setState(state);
+                contactInfo.setZip(zip);
+                contactInfo.setPhoneNo(phoneNo);
+                contactInfo.setEmail(email);
+
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
 }
